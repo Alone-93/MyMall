194005bd27c266549ba814f566ba8402e18cfc1b
Alone-93 <409433628@qq.com>
2020-08-24 11:08:47 +0800

    update

--- src/main/java/com/alone/mymall/MymallApplication.java
+++ src/main/java/com/alone/mymall/MymallApplication.java
@@ -2,15 +2,13 @@ package com.alone.mymall;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 @MapperScan( "com.alone.mymall.mbg.mapper" )
-
-
 @SpringBootApplication
 public class MymallApplication {
 
